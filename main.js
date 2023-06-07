d5c639a7d57fdbb0447be6052d54254eb0615614
elisatrovato <128367619+elisatrovato@users.noreply.github.com>
2023-06-07 15:06:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var question3 = document.quiz.question3.value;
 var question4 = document.quiz.question4.value;
 var question5 = document.quiz.question5.value;
 var question6 = document.quiz.question6.value;
-var question6 = document.quiz.question7.value;
+var question7 = document.quiz.question7.value;
 var correct = 0;
 
 
